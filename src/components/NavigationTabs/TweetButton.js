ab034cb8e2284377d101b3e936f562df05b7db5f
dzgoldman <dzgoldman@wesleyan.edu>
2020-05-21 19:36:26 -0400

    handroll tweet button instead of using react-share embed

--- src/components/NavigationTabs/TweetButton.js
+++ src/components/NavigationTabs/TweetButton.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { useWeb3Context } from 'web3-react'
-import { TwitterShareButton } from 'react-twitter-embed'
+import styled from 'styled-components'
 
+const TweetLink = styled.a`
+  position: relative;
+  box-sizing: border-box;
+  padding: 1px 8px 1px 6px;
+  background-color: #1b95e0;
+  color: #fff;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
+  text-decoration: none;
+  font-family: Helvetica;
+`
 const TweetButton = () => {
   const { account } = useWeb3Context()
+  const text = `@Arbi_Swap hey @OffChainLabs, gimmie some Ropsten test tokens plz! ${account || '0xyouraddresshere'}`
+    .split(' ')
+    .join('%20')
   return (
-    <TwitterShareButton
-      options={{
-        text: `@Arbi_Swap hey @OffChainLabs, gimmie some Ropsten test tokens plz! ${account || '0xyouraddresshere'} `,
-        size: 'small'
-      }}
-      url={'.'}
-      placeholder={'Tweet'}
-    />
+    <TweetLink target="_blank" href={`https://twitter.com/intent/tweet?text=${text}`}>
+      Tweet
+    </TweetLink>
   )
 }
 
